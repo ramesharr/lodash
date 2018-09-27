1bf7b2c27b38dd946dbb9ecbc93230e3f73c5ae2
ramesharr <ramesh1arr@gmail.com>
2018-09-27 23:55:48 +0530

    Currying added to add.js

--- add.js
+++ add.js
@@ -13,10 +13,17 @@ import createMathOperation from './.internal/createMathOperation.js'
  * add(6, 4)
  * // => 10
  */
-const add = createMathOperation((augend) =>{
-return (addend)=>{
-  return addend+augend;
-}
+const add = createMathOperation((augend)=> {
+  var sum = augend; 
+   var addNext = (addend)=> {
+   if(addend){
+      sum += addend
+      return addNext
+   }
+    else 
+      return sum;
+  }
+  addNext.toString = () => {return sum }
+  return addNext;
 }, 0)
-
 export default add
